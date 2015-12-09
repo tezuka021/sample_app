e9903b0067a6d12834b1996f366970e08f5ef93b
tezuka021 <tezuka021@sekisui.com>
2015-12-09 09:02:07 +0000

    Add a full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-  root 'static_pages#home'
+  root "static_pages#home"
   get "static_pages/help"
   get "static_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -10,6 +10,7 @@ describe "Static pages" do
     end
 
     it "should have the title 'Home'" do
+
       visit '/static_pages/home'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
     end
